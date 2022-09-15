c286c33045a43309918c736560e902e7b303ae4f
everardo277 <rene.contrerasari@nube.unadmexico.mx>
2022-09-14 22:09:11 -0500

    Primer commit

---
+++ js/script copy.js
@@ -0,0 +1,10 @@
+$(window).on("scroll", function() {
+
+   if ($(".navbar").offset().top > 40) {
+      $(".navbar").addClass("navbar-fixed");
+
+   } else {
+      $(".navbar").removeClass("navbar-fixed");
+
+   }
+})
